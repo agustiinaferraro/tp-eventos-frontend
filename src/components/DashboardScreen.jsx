bf35ded6b2e406ec2d31146aa25a9928cc0f442f
Agustina Ferraro <ferraroagustina25@gmail.com>
2026-04-14 03:09:48 -0300

    Make search bar shorter

--- src/components/DashboardScreen.jsx
+++ src/components/DashboardScreen.jsx
@@ -220,7 +220,7 @@ export default function DashboardScreen() {
         </div>
         
         {/* Buscador de salas (CENTRADO) */}
-        <div className="flex-1 max-w-md mx-4">
+        <div className="flex-1 max-w-sm mx-4">
           <div className="relative">
             <svg className="absolute left-2 top-1/2 -translate-y-1/2 w-4 h-4 text-zinc-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
